1ce3476e7b444719611520ea5421482ff2386ce5
Mathieu ROGER <mathieu.roger@kaizen-solutions.net>
2024-08-06 11:40:33 +0200

    chore: some cleanups

--- app/src/main/java/info/octera/droidstorybox/presentation/home/HomeViewModel.kt
+++ app/src/main/java/info/octera/droidstorybox/presentation/home/HomeViewModel.kt
@@ -2,10 +2,12 @@ package info.octera.droidstorybox.presentation.home
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import info.octera.droidstorybox.domain.model.pack.PackMetadata
 import info.octera.droidstorybox.domain.usecases.pack.PackUseCases
 import info.octera.droidstorybox.domain.usecases.packs.PacksUseCases
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,9 +26,11 @@ class HomeViewModel @Inject constructor(
     }
 
     fun setPackFocused(packMetadata: PackMetadata) {
-        state.value = state.value.copy(packFocused = packUseCases.getPack(packMetadata.uri))
-        state.value.packFocused?.let {
-            packUseCases.playIntro(it)
+        viewModelScope.launch {
+            state.value = state.value.copy(packFocused = packUseCases.getPack(packMetadata.uri))
+            state.value.packFocused?.let {
+                packUseCases.playIntro(it)
+            }
         }
     }
 }
