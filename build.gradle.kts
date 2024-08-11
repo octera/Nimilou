9ec260a7e85b644eda956fd1d74409e4842f539b
ROGER Mathieu <mroctera@gmail.com>
2024-08-11 18:28:41 +0200

    feat: enable Crashlytics

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
 }
 
 buildscript {
