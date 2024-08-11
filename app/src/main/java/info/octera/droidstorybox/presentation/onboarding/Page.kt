3c8a662c896465f945dafd9cb155e19179151434
ROGER Mathieu <mroctera@gmail.com>
2024-08-11 17:56:21 +0200

    feat: improve onboarding

--- app/src/main/java/info/octera/droidstorybox/presentation/onboarding/Page.kt
+++ app/src/main/java/info/octera/droidstorybox/presentation/onboarding/Page.kt
@@ -7,6 +7,7 @@ import info.octera.droidstorybox.R
 data class Page(
     val title: Int,
     val description: Int,
+    val links: Int?,
     @DrawableRes val image: Int,
 )
 
@@ -15,16 +16,19 @@ val pages =
         Page(
             title = R.string.onboarding_page1_title,
             description = R.string.onboarding_page1_desc,
+            links = null,
             image = R.drawable.icon_splash,
         ),
         Page(
             title = R.string.onboarding_page2_title,
             description = R.string.onboarding_page2_desc,
+            links = R.array.onboarding_page2_links,
             image = R.drawable.onboarding2,
         ),
         Page(
             title = R.string.onboarding_page3_title,
             description = R.string.onboarding_page3_desc,
+            links = null,
             image = R.drawable.onboarding3,
         ),
     )
--- app/src/main/java/info/octera/droidstorybox/presentation/onboarding/components/OnBoardingPage.kt
+++ app/src/main/java/info/octera/droidstorybox/presentation/onboarding/components/OnBoardingPage.kt
@@ -11,11 +11,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
 import info.octera.droidstorybox.R
 import info.octera.droidstorybox.presentation.Dimens.MediumPadding1
@@ -51,5 +58,22 @@ fun OnBoardingPage(
             style = MaterialTheme.typography.bodyMedium,
             color = colorResource(id = R.color.text_medium),
         )
+        page.links?.let {
+            stringArrayResource(id = page.links).forEach {
+                Text(
+                    buildAnnotatedString {
+                        withLink(
+                            LinkAnnotation.Url(
+                                it,
+                                TextLinkStyles(style = SpanStyle(color = Color.Blue))
+                            )
+                        ) {
+                            append(it)
+                        }
+                    }
+                )
+
+            }
+        }
     }
 }
