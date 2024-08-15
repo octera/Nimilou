0f16cfb3250cf61f1fe7f8786371d76cddedd8a0
octera <mroctera@gmail.com>
2024-08-15 13:57:23 +0200

    feat: add amazon app store release (#24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "info.octera.droidstorybox"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 32
+        versionName = "0.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
